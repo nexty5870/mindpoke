849b2329227b017a0e3d93b171132111cfaaac6f
Quentin <quentin.daems@gmail.com>
2026-01-30 19:26:01 +0100

    style: use icon-park-twotone:pin for pinned nodes

--- src/components/mindpoke/interest-graph.tsx
+++ src/components/mindpoke/interest-graph.tsx
@@ -412,7 +412,7 @@ export function InterestGraph({
                   {/* Pinned/Locked indicator */}
                   {isFixed && (
                     <div className="absolute -top-2 -left-2 w-5 h-5 flex items-center justify-center border border-[#00d4aa] bg-[#0a0a0f]">
-                      <Icon icon="ph:crosshair-simple-bold" className="w-3 h-3 text-[#00d4aa]" />
+                      <Icon icon="icon-park-twotone:pin" className="w-3 h-3 text-[#00d4aa]" />
                     </div>
                   )}
                 </div>
